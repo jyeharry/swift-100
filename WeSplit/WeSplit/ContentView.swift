7378e1c82771f0b62c0ac03218f83ef7118780cd
Jye Harry <jye.harry@nine.com.au>
2024-07-15 17:48:26 +1000

    Started WeSplit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Hello, \(name == "" ? "World" : name)!")
         }
-        .padding()
     }
 }
 
