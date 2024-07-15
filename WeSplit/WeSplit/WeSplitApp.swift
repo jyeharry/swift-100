072b2668d4ddcd631c32e4e643f36b3d3ed4b86d
Jye Harry <jye.harry@nine.com.au>
2024-07-15 16:00:40 +1000

    Initial commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jye Harry on 15/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
