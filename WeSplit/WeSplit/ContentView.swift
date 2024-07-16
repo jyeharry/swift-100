07779a68baa799dcdb669bd213340b63b588a74c
Jye Harry <jye.harry@nine.com.au>
2024-07-16 11:22:59 +1000

    Day 17 first section

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-  let students = ["Harry", "Hermione", "Ron"]
-  @State private var selectedStudent = "Harry"
+    @State private var chequeAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
 
-  var body: some View {
-    NavigationStack {
-      Form {
-        Picker("Select your student", selection: $selectedStudent) {
-          ForEach(students, id: \.self) {
-            Text($0)
-          }
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "AUD")).keyboardType(.decimalPad)
+            }
+
+            Section {
+                Text(chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "AUD"))
+            }
         }
-      }
-      .navigationTitle("Select a Student")
     }
-  }
 }
 
 #Preview {
