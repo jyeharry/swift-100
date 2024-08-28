39915c96680587ebfbaedfa8150fc260ec78d8f0
Jye Harry <jyeharry21@hotmail.com>
2024-08-28 22:56:07 +1000

    Project 5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jye Harry on 27/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
